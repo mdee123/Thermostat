ef6b3afa762360c464923db4038762ada1d3011a
Mo Deerow <mo@Mos-MacBook-Pro.local>
2016-02-16 16:52:11 +0000

    temp change

---
+++ src/thermostat.js
@@ -0,0 +1,12 @@
+function Thermostat() {
+  this.temperature = 20;
+
+}
+
+Thermostat.prototype.up = function () {
+  this.temperature += 1;
+};
+
+Thermostat.prototype.down = function () {
+  this.temperature -= 1;
+};
