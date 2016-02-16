bbf001f032663efdd1caaa994e99317590a9ed5e
Mic <mic.cassano@gmail.com>
2016-02-16 17:38:16 +0000

    Add further unit testing for Thermostat object

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -5,18 +5,87 @@ describe('Thermostat', function(){
     thermostat = new Thermostat();
   });
 
-  it('thermostat starts at 20 degrees', function(){
+  it('starts at 20 degrees', function(){
     expect(thermostat.temperature).toEqual(20);
   });
 
-  it('can increase the temperature with the up button', function(){
-    thermostat.up();
-    expect(thermostat.temperature).toEqual(21);
+  it('has a minimum temperature of 10 degrees', function () {
+    expect(thermostat.minTemp).toEqual(10);
   });
 
-  it('can decrease the temperature with the up button', function(){
-    thermostat.down();
-    expect(thermostat.temperature).toEqual(19);
+  it('starts in power saving mode', function () {
+    expect(thermostat.powerSaving).toBeTruthy();
   });
 
+  describe('#up', function () {
+    it('increases the temperature by 1 degree', function(){
+      thermostat.up();
+      expect(thermostat.temperature).toEqual(21);
+    });
+  });
+
+  describe('#down', function () {
+    it('decreases the temperature by 1 degree', function(){
+      thermostat.down();
+      expect(thermostat.temperature).toEqual(19);
+    });
+  });
+
+  describe('#switchPowerSaving', function () {
+    describe('if power saving is on', function () {
+      it('switches power saving off', function () {
+        thermostat.switchPowerSaving();
+        expect(thermostat.powerSaving).toBeFalsy();
+      });
+    });
+
+    describe('if power saving is off', function () {
+      beforeEach(function () {
+        thermostat.switchPowerSaving();
+      });
+
+      it('switches power saving on', function () {
+        thermostat.switchPowerSaving();
+        expect(thermostat.powerSaving).toBeTruthy();
+      });
+    });
+  });
+
+  describe('#reset', function () {
+    beforeEach(function () {
+      thermostat.up();
+    });
+
+    it('resets the temperature to the default value', function () {
+      thermostat.reset();
+      expect(thermostat.temperature).toEqual(20);
+    });
+  });
+
+  describe('#energyUsage', function () {
+    describe('if temperature is 25 degrees or above', function () {
+      it('returns high', function () {
+        thermostat.switchPowerSaving();
+        for (var i = 0; i < 5; i++) {
+          thermostat.up();
+        }
+        expect(thermostat.energyUsage()).toEqual(Symbol.for("high"));
+      });
+    });
+
+    describe('if temperature is 18 degrees or above', function () {
+      it('returns medium', function () {
+        expect(thermostat.energyUsage()).toEqual(Symbol.for("medium"));
+      });
+    });
+
+    describe('if temperature is below 18 degrees', function () {
+      it('returns low', function () {
+        for (var i = 0; i < 5; i++) {
+          thermostat.down();
+        }
+        expect(thermostat.energyUsage()).toEqual(Symbol.for("low"));
+      });
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,8 @@
 function Thermostat() {
   this.temperature = 20;
-
+  this.powerSaving = true;
+  this.minTemp     = 10;
+  this.maxTemp     = this.powerSaving ? 25 : 32;
 }
 
 Thermostat.prototype.up = function () {
@@ -9,4 +11,26 @@ Thermostat.prototype.up = function () {
 
 Thermostat.prototype.down = function () {
   this.temperature -= 1;
+};
+
+Thermostat.prototype.switchPowerSaving = function () {
+  this.powerSaving = !this.powerSaving;
+};
+
+Thermostat.prototype.reset = function () {
+  this.temperature = 20;
+};
+
+Thermostat.prototype.energyUsage = function () {
+  var usageLevel;
+
+  if (this.temperature >= 25) {
+    usageLevel = Symbol.for("high");
+  } else if (this.temperature >= 18) {
+    usageLevel = Symbol.for("medium");
+  } else {
+    usageLevel = Symbol.for("low");
+  }
+
+  return usageLevel;
 };
